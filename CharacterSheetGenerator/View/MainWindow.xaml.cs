8c5b2332ce8de373b3944ec66d02057746e13879
Azzarrel <t.hofmann93@gmail.com>
2019-06-09 15:43:36 +0200

    V[0.6.2.2] Bugfixing
    - weiterer Bug bei den Traits behoben
    - Erste Reihe von DataGrids angpasst und ReadOnly entfernt
    - Waffen bereits einmal um den Mode erweitert
    - Test einer neuen Print-Methode (geht aber auch nicht besser)

--- CharacterSheetGenerator/View/MainWindow.xaml.cs
+++ CharacterSheetGenerator/View/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using CharacterSheetGenerator.View;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -60,67 +61,80 @@ namespace CharacterSheetGenerator.View
         private void Button3_Click(object sender, RoutedEventArgs re)
         {
 
+            string filePath = System.IO.Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName) + "\\Saves\\test.png";
+            int width = (int)Übersicht.RenderSize.Width;
+            int height = (int)Übersicht.RenderSize.Width;
 
-            
-            System.Windows.FrameworkElement[] elements = { Übersicht as System.Windows.FrameworkElement, Fertigkeiten as System.Windows.FrameworkElement, Kampf as System.Windows.FrameworkElement,
-                                                           Zauber as System.Windows.FrameworkElement, Inventar as System.Windows.FrameworkElement,};
-            
-            PrintDialog printDialog = new PrintDialog();
-            if (printDialog.ShowDialog() == true)
+            RenderTargetBitmap renderTargetBitmap =
+            new RenderTargetBitmap(width, height, 96, 96, PixelFormats.Pbgra32);
+            renderTargetBitmap.Render(Übersicht);
+            PngBitmapEncoder pngImage = new PngBitmapEncoder();
+            pngImage.Frames.Add(BitmapFrame.Create(renderTargetBitmap));
+            using (Stream fileStream = File.Create(filePath))
             {
-
-                foreach (FrameworkElement e in elements)
-                {
-
-                    //store original scale
-                    Transform originalScale = e.LayoutTransform;
-                    //get selected printer capabilities
-                    System.Printing.PrintCapabilities capabilities = printDialog.PrintQueue.GetPrintCapabilities(printDialog.PrintTicket);
-
-  
-                    //get scale of the print wrt to screen of WPF visual
-                    double scale = Math.Min(capabilities.PageImageableArea.ExtentWidth / 1281, capabilities.PageImageableArea.ExtentHeight /
-                                   1800);
-
-                    //Transform the Visual to scale
-                    e.LayoutTransform = new ScaleTransform(scale, scale);
-
-                    //get the size of the printer page
-                    System.Windows.Size sz = new System.Windows.Size(capabilities.PageImageableArea.ExtentWidth, capabilities.PageImageableArea.ExtentHeight);
-
-                    //update the layout of the visual to the printer page size.
-                    e.Measure(sz);
-                    e.Arrange(new System.Windows.Rect(new System.Windows.Point(capabilities.PageImageableArea.OriginWidth, capabilities.PageImageableArea.OriginHeight), sz));
-
-                    //now print the visual to printer to fit on the one page.
-                    if(e.Name == "Übersicht")
-                    {
-                        printDialog.PrintVisual(Übersicht, "My Print");
-                    }
-                    if (e.Name == "Fertigkeiten")
-                    {
-                        printDialog.PrintVisual(Fertigkeiten, "My Print");
-                    }
-                    if (e.Name == "Kampf")
-                    {
-                        printDialog.PrintVisual(Kampf, "My Print");
-                    }
-                    if (e.Name == "Zauber")
-                    {
-                        printDialog.PrintVisual(Zauber, "My Print");
-                    }
-                    if (e.Name == "Inventar")
-                    {
-                        printDialog.PrintVisual(Inventar, "My Print");
-                    }
-
-
-                    //apply the original transform.
-                    e.LayoutTransform = originalScale;
-                    
-                }
-
+                pngImage.Save(fileStream);
             }
+
+
+            //System.Windows.FrameworkElement[] elements = { Übersicht as System.Windows.FrameworkElement, Fertigkeiten as System.Windows.FrameworkElement, Kampf as System.Windows.FrameworkElement,
+            //                                               Zauber as System.Windows.FrameworkElement, Inventar as System.Windows.FrameworkElement,};
+
+            //PrintDialog printDialog = new PrintDialog();
+            //if (printDialog.ShowDialog() == true)
+            //{
+
+            //    foreach (FrameworkElement e in elements)
+            //    {
+
+            //        //store original scale
+            //        Transform originalScale = e.LayoutTransform;
+            //        //get selected printer capabilities
+            //        System.Printing.PrintCapabilities capabilities = printDialog.PrintQueue.GetPrintCapabilities(printDialog.PrintTicket);
+
+
+            //        //get scale of the print wrt to screen of WPF visual
+            //        double scale = Math.Min(capabilities.PageImageableArea.ExtentWidth / 1281, capabilities.PageImageableArea.ExtentHeight /
+            //                       1800);
+
+            //        //Transform the Visual to scale
+            //        e.LayoutTransform = new ScaleTransform(scale, scale);
+
+            //        //get the size of the printer page
+            //        System.Windows.Size sz = new System.Windows.Size(capabilities.PageImageableArea.ExtentWidth, capabilities.PageImageableArea.ExtentHeight);
+
+            //        //update the layout of the visual to the printer page size.
+            //        e.Measure(sz);
+            //        e.Arrange(new System.Windows.Rect(new System.Windows.Point(capabilities.PageImageableArea.OriginWidth, capabilities.PageImageableArea.OriginHeight), sz));
+
+            //        //now print the visual to printer to fit on the one page.
+            //        if(e.Name == "Übersicht")
+            //        {
+            //            printDialog.PrintVisual(Übersicht, "My Print");
+            //        }
+            //        if (e.Name == "Fertigkeiten")
+            //        {
+            //            printDialog.PrintVisual(Fertigkeiten, "My Print");
+            //        }
+            //        if (e.Name == "Kampf")
+            //        {
+            //            printDialog.PrintVisual(Kampf, "My Print");
+            //        }
+            //        if (e.Name == "Zauber")
+            //        {
+            //            printDialog.PrintVisual(Zauber, "My Print");
+            //        }
+            //        if (e.Name == "Inventar")
+            //        {
+            //            printDialog.PrintVisual(Inventar, "My Print");
+            //        }
+
+
+            //        //apply the original transform.
+            //        e.LayoutTransform = originalScale;
+
+        //}
+
+        //    }
         
         
          

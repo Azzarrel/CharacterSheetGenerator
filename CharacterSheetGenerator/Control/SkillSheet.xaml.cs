6d309aa06cbfa0caee4ce159f5f6c4cfde4f7154
Azzarrel <t.hofmann93@gmail.com>
2019-07-06 14:58:11 +0200

    [Dev_010]  Restyling

--- CharacterSheetGenerator/Control/SkillSheet.xaml.cs
+++ CharacterSheetGenerator/Control/SkillSheet.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CharacterSheetGenerator.Helpers;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,45 @@ namespace CharacterSheetGenerator.Control
     /// </summary>
     public partial class SkillSheet : UserControl
     {
+        private SolidColorBrush m_CellColor = new SolidColorBrush();
+
+        public static readonly DependencyProperty CellColorProperty =
+            DependencyProperty.Register("CellColor", typeof(SolidColorBrush), typeof(SkillSheet),
+            new FrameworkPropertyMetadata(new SolidColorBrush(), OnCellColorPropertyChanged));
+
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public SolidColorBrush CellColor
+        {
+            get { return (SolidColorBrush)GetValue(CellColorProperty); }
+            set { SetValue(CellColorProperty, value); }
+        }
+
+        private static void OnCellColorPropertyChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            SkillSheet UserControl = obj as SkillSheet;
+            UserControl.OnPropertyChanged("CellColor");
+            UserControl.OnCellColorPropertyChanged(e);
+        }
+
+        private void OnCellColorPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            m_CellColor = CellColor;
+
+        }
+
         public SkillSheet()
         {
-             InitializeComponent();
+            CellColor = new SolidColorBrush(ColorHandler.IntToColor(15329769));
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
